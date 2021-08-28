ac69f7bd5b2bdb8f01512cc4b904156479c3c6ca
53JIlLenWe11 <ruin.of.messger@gmail.com>
2021-08-28 16:27:51 +0900

    グラフの記録を1分ごとにし、4時間分を表示するようにする

--- main/main.ino
+++ main/main.ino
@@ -98,7 +98,7 @@ void loop()
 		getViewDataTimer = now;
 	}
 
-	if (now - getDataTimer >= 30000)
+	if (now - getDataTimer >= 60000) // 1分ごとにグラフを作成
 	{
 		/* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even
 		if below background CO2 levels or above range (useful to validate sensor). You can use the
