b0c475bb5e8458dbd575acd4949357e0ae30dc3e
53JIlLenWe11 <ruin.of.messger@gmail.com>
2022-04-07 20:16:48 +0900

    フォーマット

--- main/main.ino
+++ main/main.ino
@@ -3,9 +3,9 @@
 MHZ19_uart mhz19;
 
 // Co2センサー
-#define RX_PIN 36	  // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 0	  // Tx pin which the MHZ19 Rx pin is attached to
-#define BAUDRATE 9600 // Device to MH-Z19 Serial baudrate (should not be changed)
+#define RX_PIN 36	   // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 0	   // Tx pin which the MHZ19 Rx pin is attached to
+#define BAUDRATE 9600  // Device to MH-Z19 Serial baudrate (should not be changed)
 
 #define BRIGHTNESS 8
 
@@ -19,31 +19,30 @@ unsigned long getViewDataTimer = 0;
 int history[240] = {};
 int historyPos = 0;
 
-float co2_tone_on_limit = 2000.0; // トーンを鳴らすco2の閾値
-unsigned long renderTime = 2000;  // 描画感覚のミリ秒
+float co2_tone_on_limit = 2000.0;  // トーンを鳴らすco2の閾値
+unsigned long renderTime = 2000;   // 描画感覚のミリ秒
 
-//TFT_eSPI liblary
-TFT_eSprite Lcd_buff = TFT_eSprite(&M5.Lcd); // LCDのスプライト表示ライブラリ
+// TFT_eSPI liblary
+TFT_eSprite Lcd_buff = TFT_eSprite(&M5.Lcd);  // LCDのスプライト表示ライブラリ
 
 // LCD Timer
-const uint8_t timer_mm = 10; // タイマーの分数
-const uint8_t timer_ss = 0;	 // タイマーの秒数
+const uint8_t timer_mm = 10;  // タイマーの分数
+const uint8_t timer_ss = 0;	  // タイマーの秒数
 unsigned long get_silent_timer = 0;
-const unsigned long silent_timer_limit = 600000; // タイマーの分と秒の合算ミリ秒数(10分)
+const unsigned long silent_timer_limit = 600000;  // タイマーの分と秒の合算ミリ秒数(10分)
 uint8_t mm = 0, ss = 0;
 boolean is_measuring = true;
 byte xsecs = 0, omm = 99, oss = 99;
 
-void setup()
-{
-	M5.begin(true, true, true); // LCDEnable, PowerEnable, SerialEnable(115200)
+void setup() {
+	M5.begin(true, true, true);	 // LCDEnable, PowerEnable, SerialEnable(115200)
 	// dacWrite(25, 0); // Speaker OFF
 	M5.Axp.ScreenBreath(BRIGHTNESS);
 	pinMode(M5_LED, OUTPUT);
 	M5.Lcd.setRotation(1);
-	Serial.begin(BAUDRATE); // Device to serial monitor feedback
+	Serial.begin(BAUDRATE);	 // Device to serial monitor feedback
 
-	//TFT_eSPI setup
+	// TFT_eSPI setup
 	Lcd_buff.createSprite(m5.Lcd.width(), m5.Lcd.height());
 	Lcd_buff.fillRect(0, 0, m5.Lcd.width(), m5.Lcd.height(), TFT_BLACK);
 	Lcd_buff.pushSprite(0, 0);
@@ -55,70 +54,57 @@ void setup()
 	render();
 }
 
-void loop()
-{
+void loop() {
 	auto now = millis();
 
 	// Aボタン: モードを切り替える
 	M5.update();
-	M5.Beep.update(); // tone関数で鳴らした音が指定時間経過していたら止める
+	M5.Beep.update();  // tone関数で鳴らした音が指定時間経過していたら止める
 	// Aボタン: サイレントモード
-	if (M5.BtnA.wasReleased())
-	{
+	if (M5.BtnA.wasReleased()) {
 		silent_timer_reset();
 	}
 
 	// Aボタン長押し: ゼロキャリブレーション
-	if (M5.BtnA.pressedFor(3000))
-	{
+	if (M5.BtnA.pressedFor(3000)) {
 		mhz19.calibrateZero();
 		M5.Beep.tone(1000, 100);
 	}
 
 	// Bボタン: 画面表示を ON/OFF する
-	if (M5.BtnB.wasReleased())
-	{
+	if (M5.BtnB.wasReleased()) {
 		lcdOn = !lcdOn;
 
-		if (lcdOn)
-		{
+		if (lcdOn) {
 			render();
 			M5.Axp.ScreenBreath(BRIGHTNESS);
-		}
-		else
-		{
+		} else {
 			M5.Axp.ScreenBreath(0);
 		}
 	}
 
-	if ((ledOn || !ledValue) && nextLedOperationTime <= now)
-	{
+	if ((ledOn || !ledValue) && nextLedOperationTime <= now) {
 		ledValue = !ledValue;
 		digitalWrite(M5_LED, ledValue);
 		// サイレント時間であれば音を鳴らさない
-		if (get_silent_timer <= now)
-		{
+		if (get_silent_timer <= now) {
 			M5.Beep.tone(4000, 1000);
 		}
 
 		// Lチカ
-		if (ledValue)
-		{
+		if (ledValue) {
 			nextLedOperationTime = now + 15000;
-		}
-		else
-		{
+		} else {
 			nextLedOperationTime = now + 800;
 		}
 	}
 
-	if (now - getViewDataTimer >= renderTime)
-	{
+	if (now - getViewDataTimer >= renderTime) {
 		render();
 		getViewDataTimer = now;
 	}
 
-	if (now - get_graph_timer >= 180000) // 3分ごとにグラフへ追加
+	if (now - get_graph_timer >= 180000)  // 3分ごとにグラフへ追加
 	{
 		/* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even
 		if below background CO2 levels or above range (useful to validate sensor). You can use the
@@ -132,15 +118,14 @@ void loop()
 	}
 }
 
-void render()
-{
+void render() {
 	// Clear
-	Lcd_buff.fillSprite(TFT_BLACK); // 画面を黒塗りでリセット
+	Lcd_buff.fillSprite(TFT_BLACK);	 // 画面を黒塗りでリセット
 
 	int CO2 = mhz19.getCO2PPM();
-	ledOn = CO2 >= co2_tone_on_limit; // LEDをON
+	ledOn = CO2 >= co2_tone_on_limit;  // LEDをON
 
-	if (!lcdOn) // 描画OFFであれば処理を抜ける
+	if (!lcdOn)	 // 描画OFFであれば処理を抜ける
 	{
 		return;
 	}
@@ -148,8 +133,7 @@ void render()
 	int height = m5.Lcd.height();
 	int width = m5.Lcd.width();
 	int len = sizeof(history) / sizeof(int);
-	for (int i = 0; i < len; i++)
-	{
+	for (int i = 0; i < len; i++) {
 		auto value = max(0, history[(historyPos + 1 + i) % len] - 400);
 		auto y = min(height, (int)(value * (height / co2_tone_on_limit)));
 		auto color = min(255, (int)(value * (255 / co2_tone_on_limit)));
@@ -159,11 +143,10 @@ void render()
 	Serial.println("CO2 (ppm): " + (String)CO2 + ", Temperature (C): " + (String)temp);
 	co2_text_render("CO2 : " + (String)CO2 + " ppm");
 	tone_timer_view();
-	Lcd_buff.pushSprite(0, 0); // LCDに描画
+	Lcd_buff.pushSprite(0, 0);	// LCDに描画
 }
 
-void co2_text_render(const String &string)
-{
+void co2_text_render(const String &string) {
 	Lcd_buff.setTextSize(2);
 	Lcd_buff.setTextColor(TFT_BLACK);
 	Lcd_buff.drawString(string, 11, 11, 2);
@@ -173,65 +156,50 @@ void co2_text_render(const String &string)
 	Lcd_buff.drawString(string, 10, 10, 2);
 }
 
-void silent_timer_reset()
-{
-	get_silent_timer = millis() + silent_timer_limit; // 10分サイレント
+void silent_timer_reset() {
+	get_silent_timer = millis() + silent_timer_limit;  // 10分サイレント
 	mm = timer_mm;
 	ss = timer_ss;
 }
 
-void tone_timer_view()
-{
-	if (get_silent_timer >= millis())
-	{
-		if (ss == 0 && mm == 0)
-		{
+void tone_timer_view() {
+	if (get_silent_timer >= millis()) {
+		if (ss == 0 && mm == 0) {
 			return;
-		}
-		else
-		{
+		} else {
 			ss = ss - renderTime / 1000;
 		}
-		if (ss >= 60)
-		{						  // Check for roll-over
-			ss = 59 + (ss - 255); // Reset seconds to zero
-			mm = mm - 1;		  // Advance minute
-			if (mm == 255)
-			{ // Check for roll-over
+		if (ss >= 60) {			   // Check for roll-over
+			ss = 59 + (ss - 255);  // Reset seconds to zero
+			mm = mm - 1;		   // Advance minute
+			if (mm == 255) {	   // Check for roll-over
 				mm = 0;
 			}
 		}
-	}
-	else
-	{
+	} else {
 		return;
 	}
 	// Draw digital time
 	int xpos = 60;
-	int ypos = 60; // Top left corner ot clock text, about half way down
+	int ypos = 60;	// Top left corner ot clock text, about half way down
 	int ysecs = ypos;
 	Lcd_buff.setTextColor(TFT_GREEN);
 	// Redraw hours and minutes time every minute
 	omm = mm;
-	if (mm < 10)
-		xpos += Lcd_buff.drawChar('0', xpos, ypos, 4);
+	if (mm < 10) xpos += Lcd_buff.drawChar('0', xpos, ypos, 4);
 	xpos += Lcd_buff.drawNumber(mm, xpos, ypos, 4);
 	xsecs = xpos;
 	// Redraw seconds time every second
 	oss = ss;
 	xpos = xsecs;
-	if (ss % 2)
-	{														 // Flash the colons on/off
-		Lcd_buff.setTextColor(0x39C4);						 // Set colour to grey to dim colon
-		xpos += Lcd_buff.drawChar(':', xsecs, ysecs - 8, 4); // Seconds colon
+	if (ss % 2) {											  // Flash the colons on/off
+		Lcd_buff.setTextColor(0x39C4);						  // Set colour to grey to dim colon
+		xpos += Lcd_buff.drawChar(':', xsecs, ysecs - 8, 4);  // Seconds colon
 		Lcd_buff.setTextColor(TFT_GREEN);
+	} else {
+		xpos += Lcd_buff.drawChar(':', xsecs, ysecs - 8, 4);  // Seconds colon
 	}
-	else
-	{
-		xpos += Lcd_buff.drawChar(':', xsecs, ysecs - 8, 4); // Seconds colon
-	}
-	//Draw seconds
-	if (ss < 10)
-		xpos += Lcd_buff.drawChar('0', xpos, ysecs, 4); // Add leading zero
-	Lcd_buff.drawNumber(ss, xpos, ysecs, 4);			// Draw seconds
+	// Draw seconds
+	if (ss < 10) xpos += Lcd_buff.drawChar('0', xpos, ysecs, 4);  // Add leading zero
+	Lcd_buff.drawNumber(ss, xpos, ysecs, 4);					  // Draw seconds
 }
